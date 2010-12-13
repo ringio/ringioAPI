8aedbb60f5dc08969f76a2ebe5eff4365e15d5d1
Álvaro Martín Fraguas <amartin@paradigmatecnologico.com>
2010-12-13 17:24:50 +0100

    Added Rails bug handling for notes.

--- lib/ringioAPI/note.rb
+++ lib/ringioAPI/note.rb
@@ -1,7 +1,52 @@
 module RingioAPI
   
   class Note < Base
-  
+
+    # override methods as a temporary solution of the JSON parsing bugs of Rails
+    def self.find(note_id)
+      super(note_id).note
+    end
+
+    def save
+
+      result = super
+
+      # move the fields returned after an update response to their proper place in the root
+      begin
+        self.id = self.note.id
+      rescue NoMethodError
+      end
+      begin
+        self.contact_id = self.note.contact_id
+      rescue NoMethodError
+      end
+      begin
+        self.author_id = self.note.author_id
+      rescue NoMethodError
+      end
+      begin
+        self.body = self.note.body
+      rescue NoMethodError
+      end
+      begin
+        self.last_updated = self.note.last_updated
+      rescue NoMethodError
+      end
+      begin
+        self.date_created = self.note.last_updated
+      rescue NoMethodError
+      end
+
+      # clean the unused nested resource
+      begin
+        self.note = nil
+      rescue NoMethodError
+      end
+
+      result
+    end
+
+
   end
 
 end
