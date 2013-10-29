7f894fb4ed24f0242603b9d3c6248c0367566c03
Srijak Rijal <srijak@gmail.com>
2013-10-29 15:21:26 -0400

    updated ring model too

--- lib/ringioAPI/ring.rb
+++ lib/ringioAPI/ring.rb
@@ -2,11 +2,6 @@ module RingioAPI
   
   class Ring < Base
 
-    # override method as a temporary solution of the JSON parsing bugs of Rails
-    def self.find(ring_id)
-      super(ring_id).ring
-    end
-
     def save
       raise "Cannot change Ringio rings"
     end
