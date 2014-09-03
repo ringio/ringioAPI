54466c0ee6ed52e7e9b0d58458000c513e69124c
Srijak Rijal <srijak@gmail.com>
2014-09-03 16:57:56 -0400

    minor

--- lib/ringioAPI/call.rb
+++ lib/ringioAPI/call.rb
@@ -1,8 +1,8 @@
 module RingioAPI
 
-class Call < Base
-  self.prefix = "/api/1/users/:user_id/"
-  self.include_format_in_path = false
-end
+  class Call < Base
+    self.prefix = "/api/1/users/:user_id/"
+    self.include_format_in_path = false
+  end
 
 end
